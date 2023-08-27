2d51d3e396aa0bc63f69b7ce0dccba54c81a3209
tuan loc <tuanloc288@gmail.com>
2023-08-27 23:20:38 +0700

    (finished) Dashboard UI + chart for analysis

---
+++ actions/getStockCount.ts
@@ -0,0 +1,12 @@
+import prismadb from "@/lib/prismadb";
+
+export const getStockCount = async (storeId: string) => {
+    const stockCount = await prismadb.product.count({
+        where: {
+            storeId,
+            isOutOfStock: false
+        }
+    })
+
+    return stockCount
+}
